609c23ffefc17c48b37dcf9b8585a11e37480693
Nikita <justxykc@gmail.com>
2015-11-01 14:14:01 +0000

    finished 1 part

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,15 +3,40 @@
 # Part 1
 
 def sum arr
-  return 0
+  result = 0
+  unless arr.empty?
+    arr.each { |num| result+=num}
+  end
+  return result
 end
-
+=begin
+ takes an array of integers as an argument and returns the sum of its two largest elements. For an empty array it should return zero. For an array with just one element, it should return that element.
+=end
 def max_2_sum arr
-  # YOUR CODE HERE
+  result = 0
+  unless arr.empty?
+    if arr.size == 1
+      result = arr[0]
+    else 
+      arr = arr.sort{|x,y| y<=>x}
+      result = arr[0]+arr[1]
+    end
+  end
+  return result
 end
-
+=begin
+akes an array of integers and an additional integer, n, as arguments and returns 
+true if any two elements in the array of integers sum to n. 
+An empty array should sum to zero by definition.
+=end
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  result = false
+  sum = 0
+  if arr.size>1
+    arr.each {|num| result = true if num == n}
+  end
+  
+  return result
 end
 
 # Part 2
